5a0fe6c76eb1c2f40de7f645a05645854e68086c
daikk115 <daikk115@gmail.com>
2016-09-13 17:00:10 +0700

    Update respone when create VPC

--- create_vpc.py
+++ create_vpc.py
@@ -14,3 +14,8 @@ output = client.create_vpc(
 )
 
 print output
+
+"""
+Respone:
+
+{'ResponseMetadata': {'HTTPStatusCode': 200, 'RequestId': 'req-0b7db2b4-420e-4217-9106-e8cdb4137f65', 'HTTPHeaders': {'date': 'Tue, 13 Sep 2016 09:52:24 GMT', 'content-length': '351', 'content-type': 'text/xml'}}, u'Vpc': {u'State': 'available', u'VpcId': 'vpc-5eed72c5', u'CidrBlock': '10.10.10.0/24', u'IsDefault': False, u'DhcpOptionsId': 'default'}}
