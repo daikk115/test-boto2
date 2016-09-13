1bd419832ed497c94aa595b2ff4edfc6a575accd
daikk115 <daikk115@gmail.com>
2016-09-13 12:06:12 +0700

    Add list VPCs

---
+++ describe_vpcs.py
@@ -0,0 +1,8 @@
+import boto3
+client = boto3.client('ec2',
+		aws_access_key_id='c543fa29eeaf4894a1078ec0860baefd',
+                aws_secret_access_key='d2246a2235ca40ffa7fbf817ae1108ba',
+                region_name='RegionOne',
+		endpoint_url='http://192.168.122.75:8788')
+output = client.describe_vpcs()
+print output
