1def872beea98337f9dc3cbb3b83ef6f4157c719
daikk115 <daikk115@gmail.com>
2016-11-10 10:54:26 +0700

    Add result

--- allocate_address.py
+++ allocate_address.py
@@ -6,3 +6,7 @@ client = boto3.client('ec2',
 		endpoint_url='http://192.168.122.75:8788')
 output = client.allocate_address(Domain='vpc')
 print output
+
+"""
+{u'PublicIp': '192.168.122.223', u'Domain': 'vpc', u'AllocationId': 'eipalloc-2dcd562c', 'ResponseMetadata': {'HTTPStatusCode': 200, 'RequestId': 'req-e078e96c-bec4-4365-bf80-d365d9b4cb26', 'HTTPHeaders': {'date': 'Thu, 10 Nov 2016 03:53:28 GMT', 'content-length': '279', 'content-type': 'text/xml'}}}
+"""
