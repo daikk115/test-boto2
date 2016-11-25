3b164cc71c814aa1bd1795365861920fb91a3413
daikk115 <daikk115@gmail.com>
2016-11-25 16:16:03 +0700

    Delete all vpc

--- delete_vpc.py
+++ delete_vpc.py
@@ -5,18 +5,8 @@ client = boto3.client('ec2',
                 region_name='RegionOne',
 		endpoint_url='http://192.168.122.75:8788')
 
-output_subnet = client.delete_subnet(SubnetId='subnet-9dcb6b38')
-output_vpc = client.delete_vpc(VpcId='vpc-5eed72c5')
+vpcs = client.describe_vpcs().get('Vpcs')
+for vpc in vpcs:
+    print "deleted"
+    print client.delete_vpc(VpcId=vpc['VpcId'])
 
-print output_subnet
-print output_vpc
-
-"""
-Respone
-
-{'ResponseMetadata': {'HTTPStatusCode': 200, 'RequestId': 'req-ce0ec162-8d2e-449f-a824-ca3fb6665b88', 'HTTPHeaders': {'date': 'Tue, 13 Sep 2016 11:02:45 GMT', 'content-length': '186', 'content-type': 'text/xml'}}}
-{'ResponseMetadata': {'HTTPStatusCode': 200, 'RequestId': 'req-51ff0526-270e-48bd-bba5-46a84a5ff152', 'HTTPHeaders': {'date': 'Tue, 13 Sep 2016 11:02:46 GMT', 'content-length': '180', 'content-type': 'text/xml'}}}
-
-NOTE: we must delete subnet before we want to delete vpc. 
-This is different from OpenStack that I can remove network without deleting subnet
-"""
