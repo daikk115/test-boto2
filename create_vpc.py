45bd0125e179f78df50f2a8d64d27eaea40d165a
daikk115 <daikk115@gmail.com>
2016-12-09 14:38:59 +0700

    lossing three double quotes

--- create_vpc.py
+++ create_vpc.py
@@ -6,7 +6,7 @@ client = boto3.client('ec2',
 		endpoint_url='http://192.168.122.75:8788')
 
 dr = True
-cidr = '10.10.10.0/24'
+cidr = '11.11.11.0/24'
 
 output = client.create_vpc(
     CidrBlock=cidr,
@@ -19,3 +19,4 @@ print output
 Respone:
 
 {'ResponseMetadata': {'HTTPStatusCode': 200, 'RequestId': 'req-0b7db2b4-420e-4217-9106-e8cdb4137f65', 'HTTPHeaders': {'date': 'Tue, 13 Sep 2016 09:52:24 GMT', 'content-length': '351', 'content-type': 'text/xml'}}, u'Vpc': {u'State': 'available', u'VpcId': 'vpc-5eed72c5', u'CidrBlock': '10.10.10.0/24', u'IsDefault': False, u'DhcpOptionsId': 'default'}}
+"""
