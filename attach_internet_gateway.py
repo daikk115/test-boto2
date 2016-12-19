829fe035f9bbe7619bd3dee22e87ad7f03f49699
daikk115 <daikk115@gmail.com>
2016-12-20 02:18:00 +0700

    iterface

---
+++ attach_internet_gateway.py
@@ -0,0 +1,20 @@
+import boto3
+from botocore.exceptions import ClientError
+
+from env import *
+
+client = boto3.client('ec2',
+                aws_access_key_id=access,
+                aws_secret_access_key=secret,
+                region_name='RegionOne',
+                endpoint_url=endpoint)
+
+results = client.attach_internet_gateway(
+    InternetGatewayId='igw-6198c1a5',
+    VpcId='vpc-624be7bb'
+)
+print results
+
+"""
+
+"""
