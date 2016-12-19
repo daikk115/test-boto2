90f1505c861723c1ea7890ab9dc1f4f356354039
daikk115 <daikk115@gmail.com>
2016-12-20 02:31:23 +0700

    attach/ not attach des

--- describe_internet_gateways.py
+++ describe_internet_gateways.py
@@ -13,5 +13,10 @@ results = client.describe_internet_gateways()
 print results
 
 """
+If not attach with any VPC
 {u'InternetGateways': [{u'InternetGatewayId': 'igw-6198c1a5', u'Attachments': []}], 'ResponseMetadata': {'RetryAttempts': 0, 'HTTPStatusCode': 200, 'RequestId': 'req-40fe0edb-ed79-4296-9dcb-413af96d8973', 'HTTPHeaders': {'date': 'Mon, 19 Dec 2016 18:55:38 GMT', 'content-length': '337', 'content-type': 'text/xml'}}}
+
+If have just attached
+{u'InternetGateways': [{u'InternetGatewayId': 'igw-6198c1a5', u'Attachments': [{u'State': 'available', u'VpcId': 'vpc-624be7bb'}]}], 'ResponseMetadata': {'RetryAttempts': 0, 'HTTPStatusCode': 200, 'RequestId': 'req-d34738a6-e130-4b85-8f07-f0282a057872', 'HTTPHeaders': {'date': 'Mon, 19 Dec 2016 19:30:19 GMT', 'content-length': '463', 'content-type': 'text/xml'}}}
+
 """
